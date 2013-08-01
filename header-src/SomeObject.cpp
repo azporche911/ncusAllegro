39da9dbc35361c370b303eefc91222f5117ccf74
sendyHalim <sendy_ubm@yahoo.com>
2013-08-02 00:06:30 +0700

    First commit with basic display class

---
+++ header-src/SomeObject.cpp
@@ -0,0 +1,52 @@
+#include "../header/SomeObject.h"
+SomeObject::SomeObject()
+{
+    al_init(); // I'm not checking the return value for simplicity.
+    al_init_image_addon();
+    al_init_primitives_addon();
+}
+
+SomeObject* SomeObject::setDisplay(int w, int h)
+{
+    width = w;
+    height = h;
+
+    return this;
+}
+
+SomeObject* SomeObject::fullScreen()
+{
+    ALLEGRO_DISPLAY_MODE disp_data;
+    al_get_display_mode(al_get_num_display_modes() - 1, &disp_data);
+
+    width = disp_data.width;
+    height = disp_data.height;
+
+    al_set_new_display_flags(ALLEGRO_FULLSCREEN);
+
+    return this;
+}
+
+SomeObject* SomeObject::delay(double t)
+{
+    al_rest(t);
+
+    return this;
+}
+
+SomeObject* SomeObject::destroyDisplay()
+{
+    al_destroy_display(display);
+
+    return this;
+}
+
+SomeObject* SomeObject::draw()
+{
+     display = al_create_display(width, height);
+}
+
+void SomeObject::printSomething()
+{
+    std::cout<<"im here!";
+}
